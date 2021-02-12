63f60e876b67801d5259323e9988b63542d9229a
haldix <hjdpirate@yahoo.com>
2021-02-11 21:24:20 -0500

    intial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+https://www.youtube.com/watch?v=vUe91uOx7R0&t=1s
---
+++ src/firebase/config.js
@@ -0,0 +1,21 @@
+import * as firebase from 'firebase/app';
+import 'firebase/storage';
+import 'firebase/firestore';
+
+var firebaseConfig = {
+  apiKey: 'AIzaSyC50FMM8sjI_QON_PUzqhd_J_wL3wJQL3I',
+  authDomain: 'firegram-65a7c.firebaseapp.com',
+  projectId: 'firegram-65a7c',
+  storageBucket: 'firegram-65a7c.appspot.com',
+  messagingSenderId: '875341466113',
+  appId: '1:875341466113:web:5ceb3a7e291180e1094e00',
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const projectStorage = firebase.storage();
+const projectFirestore = firebase.firestore();
+const timestamp = firebase.firestore.FieldValue.serverTimestamp;
+
+export { projectStorage, projectFirestore, timestamp };
