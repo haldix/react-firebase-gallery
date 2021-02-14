aefb91c5c30d3343a0704ab127cfa05ddd7ecbfd
haldix <hjdpirate@yahoo.com>
2021-02-13 19:55:36 -0500

    chg input handling from useRef to state

--- src/comps/Signup.js
+++ src/comps/Signup.js
@@ -1,14 +1,16 @@
-import React, { useRef, useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../form.scss';
 import { useAuth } from '../contexts/AuthContext';
 import toast from 'react-hot-toast';
+const initValues = {
+  email: '',
+  password: '',
+  confirmPassword: '',
+};
 
 const Signup = () => {
   const { signup } = useAuth();
-  const emailRef = useRef();
-  const passwordRef = useRef();
-  const formRef = useRef();
-  const confirmPasswordRef = useRef();
+  const [values, setValues] = useState(initValues);
   const [error, setError] = useState('');
   const [loading, setLoading] = useState(false);
 
@@ -18,18 +20,23 @@ const Signup = () => {
     }
   }, [error]);
 
+  const handleChange = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    if (passwordRef.current.value !== confirmPasswordRef.current.value) {
+    if (values.password !== values.confirmPassword) {
       console.log('Mismatch pws');
       return setError('Passwords do not match.');
     }
     try {
       setError('');
       setLoading(true);
-      await signup(emailRef.current.value, passwordRef.current.value);
-      formRef.current.reset();
+      await signup(values.email, values.password);
+      setValues(initValues);
+      toast.success('Account created successfully!');
     } catch {
       setError('Failed to create an account');
     }
@@ -39,18 +46,36 @@ const Signup = () => {
   return (
     <div className='signup'>
       <h2>Sign Up</h2>
-      <form ref={formRef} onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit}>
         <div className='input-group'>
           <label htmlFor='email'>Email</label>
-          <input type='email' ref={emailRef} required />
+          <input
+            type='email'
+            name='email'
+            value={values.email}
+            required
+            onChange={handleChange}
+          />
         </div>
         <div className='input-group'>
           <label htmlFor='password'>Password</label>
-          <input type='password' ref={passwordRef} required />
+          <input
+            type='password'
+            name='password'
+            value={values.password}
+            required
+            onChange={handleChange}
+          />
         </div>
         <div className='input-group'>
           <label htmlFor='confirm-password'>Password</label>
-          <input type='password' ref={confirmPasswordRef} required />
+          <input
+            type='password'
+            name='confirmPassword'
+            value={values.confirmPassword}
+            required
+            onChange={handleChange}
+          />
         </div>
         <button className='btn-submit' type='submit' disabled={loading}>
           Sign Up
